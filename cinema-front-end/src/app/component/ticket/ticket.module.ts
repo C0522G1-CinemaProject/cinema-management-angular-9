032fb99786be531f32bd2007ac4d0bb602b62e06
huynhtrungthuyen <huynhtrungthuyen@gmail.com>
2022-11-17 15:22:03 +0700

    gop 17-11

--- cinema-front-end/src/app/component/ticket/ticket.module.ts
+++ cinema-front-end/src/app/component/ticket/ticket.module.ts
@@ -1,15 +1,9 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
-
-import {TicketRoutingModule} from './ticket-routing.module';
-import {ConfirmBookingTicketComponent} from './confirm-booking-ticket/confirm-booking-ticket.component';
-import {PaymentBookingTicketComponent} from './payment-booking-ticket/payment-booking-ticket.component';
-
-import { TicketRoutingModule } from './ticket-routing.module';
 import { BookingSeatComponent } from './booking-seat/booking-seat.component';
 import { BookingTicketComponent } from './booking-ticket/booking-ticket.component';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-
+import {TicketRoutingModule} from "./ticket-routing.module";
 
 @NgModule({
   declarations: [BookingSeatComponent, BookingTicketComponent],
